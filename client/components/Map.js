e7718f2f0b6e7135c9a7306a65072750fcaa4430
Xuming Shi <30730346+karin6543@users.noreply.github.com>
2021-05-13 11:33:46 -0400

    added textsearch

--- client/components/Map.js
+++ client/components/Map.js
@@ -25,7 +25,13 @@ const Map = withScriptjs(
   
   const getPlaces = async (lat, lng) => {
     try {
+      // default - nearby
       const { data } = await Axios.get(`https://cors-anywhere.herokuapp.com/https://maps.googleapis.com/maps/api/place/nearbysearch/json?type=cafe&input=coffee&inputtype=textquery&fields=photos,formatted_address,name,opening_hours,rating&rankby=distance&location=${lat},${lng}&key=${process.env.GOOGLE_MAPS_API_KEY}`);
+      console.log('nearby..', data)
+      // if no result from edge - textSearch
+      if(!data){
+        data  = await Axios.get(`https://cors-anywhere.herokuapp.com/https://maps.googleapis.com/maps/api/place/textsearch/json?&input=coffee&inputtype=textquery&type=cafe&fields=photos,formatted_address,name,opening_hours,rating&radius=1000&location=${lat},${lng}&key=KEY`).data;
+      }
 		 const places = data.results.filter(place => !place.types.includes("gas_station") && place.rating > 3.8).slice(0, 3);
 		setTopPlaces(places)
 		
