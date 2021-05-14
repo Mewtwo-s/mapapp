793a5259b4de4a4355ba373c444d3a0490bcd19a
Xuming Shi <30730346+karin6543@users.noreply.github.com>
2021-05-14 18:08:04 -0400

    added google api route

--- server/api/index.js
+++ server/api/index.js
@@ -4,6 +4,7 @@ module.exports = router
 router.use('/users', require('./users'))
 router.use('/sessions', require('./sessions'));
 router.use('/usersessions', require('./userSessions'));
+router.use('/google', require('./googlemaps'));
 
 router.use((req, res, next) => {
   const error = new Error('Not Found')
