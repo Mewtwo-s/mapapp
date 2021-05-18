2b2470835d4f6580bbeb10dad825e69c0309f18b
Xuming Shi <30730346+karin6543@users.noreply.github.com>
2021-05-18 00:28:25 -0400

    feat/select meetup loc

---
+++ client/components/Place.js
@@ -0,0 +1,21 @@
+import React ,{ useState, useEffect } from 'react'
+
+function Place(props) {
+    const [selection, setSelection] = useState('')
+    const {handle, location, name, open, price, rating} = props
+    
+   async function onTrigger() {
+       handle(location)
+    }
+    return (
+        <div>
+            <h4>{name}</h4>
+            <div>Open: {open}</div>
+            <div>$$$: {price}</div>
+            <div>Rating: {rating}</div>
+            <button onClick={onTrigger}>Select</button>
+        </div>
+    )
+}
+
+export default Place
