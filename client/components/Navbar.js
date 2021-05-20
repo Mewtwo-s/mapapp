9dc4e4bd38cc03bd04cba748a837da3a0cf2001b
Xuming Shi <30730346+karin6543@users.noreply.github.com>
2021-05-20 15:39:15 -0400

    fix globalStyle camcel case

--- client/components/Navbar.js
+++ client/components/Navbar.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import styled from 'styled-components'
-import { Container } from '../Globalstyles';
+import { Container } from '../GlobalStyles';
 import {connect} from 'react-redux'
 import {Link} from 'react-router-dom'
 import {logout} from '../store'
