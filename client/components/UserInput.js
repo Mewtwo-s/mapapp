1965ae35b86e46ab751ac6196bf623b79592daaa
Xuming Shi <30730346+karin6543@users.noreply.github.com>
2021-05-23 12:20:56 -0400

    user input geolocation

---
+++ client/components/UserInput.js
@@ -0,0 +1,34 @@
+import React from 'react'
+
+function UserInput(props) {
+    const {handle} = props
+
+    function handleSubmit(evt){
+        evt.preventDefault()
+        handle(evt.target.address.value)
+    }
+    return (
+        <div>
+            <h2>You are annoying, aren't you?</h2>
+            <h4>Type your address in the searc box (currently default to Central Park)</h4>
+      
+            <form name="join" onSubmit={handleSubmit}>
+           
+                <div>
+                <label htmlFor="address">
+                    <small>Type Your Address</small>
+                </label>
+                <input name="address" type="text" />
+                </div>
+                <div>
+
+                <button type='submit'> Location Me!</button>
+                </div>
+               
+                </form>
+              
+        </div>
+    )
+}
+
+export default UserInput
