df6550242979d6912300bc63b9bcd07f60c4590b
wisch628 <hannahwischnia@gmail.com>
2021-05-23 14:55:41 -0400

    removed users from gitignore file

---
+++ script/usersSeed.js
@@ -0,0 +1,40 @@
+// users for seed.js
+module.exports = [
+    { email: 'mac@mapapp.com',
+     password: '123',
+     firstName:'mac',
+     lastName:'mac',
+     phoneNum:'888-888-8888',
+     photo: 'https://fsb.zobj.net/crop.php?r=7v0JuKXUyajHe8smkJf2uYwF85brYQv3ktye9d2Q6CxArgCAuaBsliaI-JCrPBVm1Ez8_pGjiDCzW3PKLIkwLJQADwg6o5SHzA_Zlf5N3uReTO2q78fWpGFSFiRnESwLeTqV6Mpsev3qdEu_'
+   },
+    {
+       email: 'joe@mapapp.com',
+       password: '123',
+       firstName: 'joe',
+       lastName: 'joe',
+       phoneNum: '888-888-8881',
+    },
+    {
+       email: 'hannah@mail.com',
+       password: 'hannah123',
+       firstName: 'Hannah',
+       lastName: 'Smith',
+       phoneNum: '111-111-1111',
+       photo: 'https://news.kleinisd.net/wp-content/uploads/2018/09/Spelling-Bee-LISA-ROBINSON.png'
+    },
+    {
+       email: 'iris@mail.com',
+       password: 'iris123',
+       firstName: 'Iris',
+       lastName: 'Smith',
+       phoneNum: '222-222-2222',
+       photo: 'https://12ax7web.s3.amazonaws.com/accounts/1/products/1986199880060/Boba-Cat_800x800_SEPS-1000x1000.jpg'
+    },
+    {
+       email: 'kygo@mail.com',
+       password: 'kygo123',
+       firstName: 'kygo',
+       lastName: 'corgi',
+       photo: 'https://www.clipartmax.com/png/middle/452-4526050_cute-pup-kawaii-tumblr-interesting-art-heart-cute-corgi-drawings.png'
+    }
+ ]
