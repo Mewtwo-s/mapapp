076e07d9c2c4516f23b3c2563017f97be56cc928
wisch628 <hannahwischnia@gmail.com>
2021-05-23 18:12:41 -0400

    maybed fixed webpack issue plus started fixing the sessions not updating

---
+++ client/store/allSessions.js
@@ -0,0 +1,58 @@
+import axios from 'axios';
+import { sessionStarted, clearAllLocations } from './locationSharing';
+import socket from '../socket';
+
+const GET_ALL_SESSIONS = 'GET_ALL_SESSIONS';
+const ADD_A_SESSION = 'ADD_A_SESSION';
+const EDIT_A_SESSION = 'EDIT_A_SESSION';
+
+//action creators
+const getAllSessions = (sessions) => {
+  return {
+    type: GET_ALL_SESSIONS,
+    sessions,
+  };
+};
+
+export const addASession = (session) => {
+  return {
+    type: ADD_A_SESSION,
+    session,
+  };
+};
+
+export const editASession = (session) => {
+  return {
+    type: EDIT_A_SESSION,
+    session,
+  };
+};
+
+export const getAllSessionsThunkCreator = (userId) => {
+
+  return async (dispatch) => {
+    try {
+      const response = await axios.get(`/api/sessions/allsessions/${userId}`);
+      const sessions = response.data;
+      await dispatch(getAllSessions(sessions));
+
+    } catch (err) {
+      console.error(`Could not get user's sessions`, err);
+    }
+  };
+};
+
+
+export default function sessionReducer(sessions = [], action) {
+  switch (action.type) {
+    case GET_ALL_SESSIONS:
+      return action.sessions;
+    case ADD_A_SESSION:
+      return [...sessions, action.session];
+    case EDIT_A_SESSION:
+      return sessions.map((session) => 
+      session.id === action.session.id ? action.session : session);
+    default:
+      return sessions;
+  }
+}
--- webpack.config.js
+++ webpack.config.js
@@ -69,6 +69,9 @@ module.exports = {
     ]
   },
   resolve: {
+    alias: {
+      process: "process/browser"
+   },
     fallback: {
       "buffer": false,
       "stream": false,
