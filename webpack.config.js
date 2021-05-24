6941c7dddb2e28b4eb0374d09289d1be99a61811
Xuming Shi <30730346+karin6543@users.noreply.github.com>
2021-05-24 11:34:35 -0400

    webpack attempt1

--- webpack.config.js
+++ webpack.config.js
@@ -7,11 +7,6 @@ if (process.env.NODE_ENV !== 'production') {
 } else {
   environmentConfig = new DefinePlugin({
     'process.env.GOOGLE_MAPS_API_KEY': JSON.stringify(process.env.GOOGLE_MAPS_API_KEY),
-    'process.env.MAIL_USERNAME': JSON.stringify(process.env.MAIL_USERNAME),
-    'process.env.MAIL_PASSWORD': JSON.stringify(process.env.MAIL_PASSWORD),
-    'process.env.OAUTH_CLIENTID': JSON.stringify(process.env.OAUTH_CLIENTID),
-    'process.env.OAUTH_CLIENT_SECRET': JSON.stringify(process.env.OAUTH_CLIENT_SECRET),
-    'process.env.OAUTH_REFRESH_TOKEN': JSON.stringify(process.env.OAUTH_REFRESH_TOKEN),
   });
 }
 module.exports = {
@@ -32,63 +27,39 @@ module.exports = {
         },
       },
       {
-        test: /\.(png)$/,
-        use: {
-          loader: 'url-loader',
-        },
-      },
-      {
-        test: /\.(png|jpe?g|gif)$/i,
-        use: [
-          {
-            loader: 'file-loader',
-          },
-        ],
-      },
-      {
         test: /\.(sass|scss|css)$/,
         use: ['style-loader', 'css-loader', 'sass-loader'],
-      },
-      {
-        test: /\.(svg|eot|woff|woff2|ttf)(\?v=\d+\.\d+\.\d+)?$/,
-        use: {
-          loader: 'file-loader',
-          options: {
-            name: 'public/map-logo.png',
-            outputPath: 'fonts/',
-          },
-        },
       },
     ],
   },
   resolve: {
-    alias: {
-      process: 'process/browser',
-    },
-    fallback: {
-      buffer: false,
-      stream: false,
-      net: false,
-      path: false,
-      zlib: false,
-      http: false,
-      https: false,
-      crypto: false,
-      cheerio: false,
-      parse5: false,
-      constants: false,
-      assert: false,
-      fs: false,
-      url: false,
-      vm: false,
-      querystring: false,
-      os: false,
-      child_process: false,
-      worker_threads: false,
-      inspector: false,
-      process: false,
-      util: require.resolve('util/'),
-    },
+    // alias: {
+    //   process: 'process/browser',
+    // },
+    // fallback: {
+    //   buffer: false,
+    //   stream: false,
+    //   net: false,
+    //   path: false,
+    //   zlib: false,
+    //   http: false,
+    //   https: false,
+    //   crypto: false,
+    //   cheerio: false,
+    //   parse5: false,
+    //   constants: false,
+    //   assert: false,
+    //   fs: false,
+    //   url: false,
+    //   vm: false,
+    //   querystring: false,
+    //   os: false,
+    //   child_process: false,
+    //   worker_threads: false,
+    //   inspector: false,
+    //   process: false,
+    //   util: require.resolve('util/'),
+    // },
   },
   plugins: [environmentConfig],
 };
